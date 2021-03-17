b9a8035ec32e86019663a68726e7fe42308cd5f0
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-03-16 19:26:30 -0700

    Change element in array list.

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    cars.set(3, "Mercedes");
+    System.out.println(cars);
+  } 
+}
